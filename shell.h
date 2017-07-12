5fbcadaa0855e4ddeee9fdd5cbbf1a88f8d3c10a
alexaorrico <alexaorrico@gmail.com>
2017-07-12 17:33:43 +0000

    added function prototypes

--- shell.h
+++ shell.h
@@ -10,9 +10,6 @@
 #include <sys/wait.h>
 
 extern char **environ;
-char *_strdup(char *strtodup);
-int _strcmpr(char *strcmp1, char *strcmp2);
-char *_strcat(char *strc1, char *strc2);
 
 /**
  * struct environment - struct for a linked list of environment variables
@@ -37,5 +34,14 @@ typedef struct builtins
 	char *name;
 	void (*f)(char *[], env_t **);
 } builtins_t;
+
+char *_strdup(char *strtodup);
+int _strcmpr(char *strcmp1, char *strcmp2);
+char *_strcat(char *strc1, char *strc2);
+ssize_t _puts(char *str);
+char **tokenize_av(char *buffer);
+void (*check_for_builtins(char *av[], env_t **env))(char *[], env_t **);
+int check_for_path(char *av[], env_t **env);
+
 
 #endif /* _SHELL_H_ */
