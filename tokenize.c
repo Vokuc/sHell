9102dde2bedee1e94796b4f86531adae2d2f5291
alexaorrico <alexaorrico@gmail.com>
2017-07-12 17:19:48 +0000

    tokenization function for av

---
+++ tokenize.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+
+char **tokenize_av (char *buffer)
+{
+	char **av;
+	size_t i, mcount = 10;
+
+	av = malloc(sizeof(char *) * mcount);
+	if (av == NULL)
+	{
+		perror("Fatal Error");
+		return (NULL);
+	}
+	while ((av[i] = strtok(buffer, "\n \t")) != NULL)
+	{
+		i++;
+		if (i == mcount)
+		{
+			av = _realloc(av, &mcount, sizeof(char *));
+			if (av == NULL)
+			{
+				perror("Fatal Error");
+				return (NULL);
+			}
+		}
+		buffer = NULL;
+	}
+}
